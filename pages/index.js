132a0fe09c8a8f1e77e6fa1e062f54452d0e67c2
AlbertoIJS <aijs.cdmx@gmail.com>
2021-02-18 17:17:05 -0600

    For him, for her, latest post sections added and container class removed from new collection

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 
 import NewCollection from '../components/newCollection'
 import FeaturedProducts from '../components/featuredProducts'
+import ForHimHer from '../components/forhimher'
+import LatestPosts from '../components/latestPosts'
 
 export default function Home() {
   return (
@@ -9,10 +11,12 @@ export default function Home() {
       <Head>
         <title>Fashion Nova Clone Home</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous" />
       </Head>
       <NewCollection />
       <FeaturedProducts />
+      <ForHimHer />
+      <LatestPosts />
     </div>
   )
 }
