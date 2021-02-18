148c9b50de8c4c3d099bf8b0913aaf83e1eeba8f
WebDevHod <webdevhod@gmail.com>
2021-02-18 03:59:19 -0800

    Started collections page, gave it a fluid container and two columns with a search filter and product gallery

--- pages/collections.js
+++ pages/collections.js
@@ -1,15 +1,42 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Head from "next/head";
 
 const Collections = () => {
   return (
-    <div>
+    <>
         <Head>
             <title>Collections</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous"/>
         </Head>
-
-        <h1>Welcome to the Collections Page!</h1>
-    </div>
+        <section id="#collections" className="collections">
+          <div className="container-fluid collections__container">
+            <h1 className="collections__header">Spring Collections</h1>
+            <div class="row">
+              <div class="col-3 collections__search-filter search-filter">
+                <ul>
+                  <li>Color</li>
+                  <li>Size</li>
+                  <li>Price</li>
+                </ul>
+              </div>
+              <div class="col-9 collections__gallery gallery">
+                <div class="row">
+                  <div class="col-3 gallery__column"></div>
+                  <div class="col-3 gallery__column"></div>
+                  <div class="col-3 gallery__column"></div>
+                  <div class="col-3 gallery__column"></div>
+                </div>
+                <div class="row">
+                  <div class="col-3 gallery__column"></div>
+                  <div class="col-3 gallery__column"></div>
+                  <div class="col-3 gallery__column"></div>
+                  <div class="col-3 gallery__column"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+    </>
   );
 };
 
