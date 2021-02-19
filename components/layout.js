66554b711dae4790f2497341f3bfd56e1c8bbad2
Patricia Pires Dias <ppiresdias@umassd.edu>
2021-02-19 17:50:26 -0500

    removed unused resources

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,10 @@
 import Footer from '../components/footer';
 import Header from '../components/header'
-import Header2 from '../components/header2'
 
 export default function Layout(props) {
 	return (
 		<div>
-			<Header2 />
+			<Header />
 			<div>{props.children}</div>
 			<Footer />
 		</div>
