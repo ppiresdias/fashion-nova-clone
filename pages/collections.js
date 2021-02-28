024af7e50294a02c4665d62b3ea349469f06e862
WebDevHod <webdevhod@gmail.com>
2021-02-28 00:58:39 -0800

    Removed an extra nested filter section. Added extra text to demonstrate text-overflow ellipsis.

--- pages/collections.js
+++ pages/collections.js
@@ -20,7 +20,7 @@ const Collections = () => {
   // Product component, loops for now
   const getPhotos = (count=10) => {
     let arr = [];
-    let photos = {0: ["Hold on to this vibe tie dye Crop Top - Pink/combo", "001-top1-HoldOnToThisVibeTieDyeCropTop-Pinkcombo_MER.jpg", "001-top2-07-20-20Studio4_EF_CP_11-51-59_36_T8134_PinkCombo_2982_JK_360x.webp"], 1: ["Grind Time Beyond", "002-top1-11-07-19_Studio_1_DV_KYS_14-11-53_24__BNL1117_Nude__7_EH.jpg", "002-top2-11-07-19_Studio_1_DV_KYS_14-11-53_24__BNL1117_Nude__14_EH_360x.jpg"]};
+    let photos = {0: ["Hold on to this vibe tie dye Crop Top - Pink/combo", "001-top1-HoldOnToThisVibeTieDyeCropTop-Pinkcombo_MER.jpg", "001-top2-07-20-20Studio4_EF_CP_11-51-59_36_T8134_PinkCombo_2982_JK_360x.webp"], 1: ["Grind Time Beyond Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti odio eos culpa dignissimos, laborum fuga, maxime enim recusandae, perferendis consectetur nam!", "002-top1-11-07-19_Studio_1_DV_KYS_14-11-53_24__BNL1117_Nude__7_EH.jpg", "002-top2-11-07-19_Studio_1_DV_KYS_14-11-53_24__BNL1117_Nude__14_EH_360x.jpg"]};
     
     for(let i = 0; i < count; ++i) {
       let index = i % 2;
@@ -57,9 +57,7 @@ const Collections = () => {
             <div className="row">
               <div className="d-none d-md-block col-md-3">
                 <div className="container">
-                  <section className="filter">
                     <Filter />
-                  </section>
                 </div>
               </div>
               <div className="col-12 col-md-9 collections__gallery gallery">
