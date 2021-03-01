007e1ca52abd427ced34b074030751507b019091
WebDevHod <webdevhod@gmail.com>
2021-02-28 17:23:02 -0800

    Restored the bootstrap npm dependency.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "../styles/style.scss";
+
 import Layout from "../components/layout";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
   return (
