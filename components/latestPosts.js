8e5d07af48d6c6c513e46015f4ae73c245a3448b
AlbertoIJS <aijs.cdmx@gmail.com>
2021-03-02 11:13:47 -0600

    Set fixed height to latest posts thumbnails

--- components/latestPosts.js
+++ components/latestPosts.js
@@ -4,7 +4,7 @@ export default function LastestPosts() {
       <h2>Latest posts</h2>
       <div className="row">
         <div className="card col-12 col-md-4">
-          <img loading="lazy" src="https://source.unsplash.com/0O6Fv3Ff_XI" />
+          <img loading="lazy" src="https://source.unsplash.com/0O6Fv3Ff_XI/350x360" />
           <div className="card-body">
             <h5 className="card-title">Best Outfits Of The Month</h5>
             <p className="card-text">
@@ -17,7 +17,7 @@ export default function LastestPosts() {
           </div>
         </div>
         <div className="card col-12 col-md-4">
-          <img loading="lazy" src="https://source.unsplash.com/Eav5V0COmQ4/350x379" />
+          <img loading="lazy" src="https://source.unsplash.com/Eav5V0COmQ4/350x360" />
           <div className="card-body">
             <h5 className="card-title">Best Outfit For Men</h5>
             <p className="card-text">
@@ -29,7 +29,7 @@ export default function LastestPosts() {
           </div>
         </div>
         <div className="card col-12 col-md-4">
-          <img loading="lazy" src="https://source.unsplash.com/0AAiQq6rhlw" />
+          <img loading="lazy" src="https://source.unsplash.com/0AAiQq6rhlw/350x360" />
           <div className="card-body">
             <h5 className="card-title">Latest Spring Trends</h5>
             <p className="card-text">
